cbe10c518b26668888a31cffa7fc58d01aa6f7c6
Martin Atkins <mart@degeneration.co.uk>
2018-02-24 12:48:28 -0800

    constraints: VersionSpec to implement Stringer

--- versions/constraints/spec.go
+++ versions/constraints/spec.go
@@ -1,9 +1,10 @@
 package constraints
 
-type NumConstraint struct {
-	Num           uint64
-	Unconstrained bool
-}
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
 
 type VersionSpec struct {
 	Major      NumConstraint
@@ -11,4 +12,29 @@ type VersionSpec struct {
 	Patch      NumConstraint
 	Prerelease string
 	Metadata   string
+}
+
+func (s VersionSpec) String() string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "%s.%s.%s", s.Major, s.Minor, s.Patch)
+	if s.Prerelease != "" {
+		fmt.Fprintf(&buf, "-%s", s.Prerelease)
+	}
+	if s.Metadata != "" {
+		fmt.Fprintf(&buf, "+%s", s.Metadata)
+	}
+	return buf.String()
+}
+
+type NumConstraint struct {
+	Num           uint64
+	Unconstrained bool
+}
+
+func (c NumConstraint) String() string {
+	if c.Unconstrained {
+		return "*"
+	} else {
+		return strconv.FormatUint(c.Num, 10)
+	}
 }
